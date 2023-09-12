66a8f9c711bf873248aeda1504cdf29c7c7dab96
student <student@MB>
2023-09-12 10:40:08 +0530

    Add project files.

---
+++ webapi2/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace webapi2
+{
+    public class Employee
+    {
+        public int Id { set; get; }
+        public string? Name { set; get; }
+
+        public string Place { set; get; } = string.Empty;
+
+    }
+}
---
+++ webapi2/data/EmpContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webapi2;
+
+namespace webapi2.data
+{
+    public class EmpContext : DbContext
+    {
+        public EmpContext(DbContextOptions<EmpContext> options) : base(options)
+
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
+
+
+
+
